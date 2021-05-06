2d112ba15495ecec583eec85ac99f9b8adc57b8c
Muhammad Rehan <mrmuhammadrehan65@gmail.com>
2021-05-06 10:07:59 +0400

    complete app

--- api/routes/employee.js
+++ api/routes/employee.js
@@ -18,8 +18,164 @@ let rules = [
 ];
 
 
+/**
+ * @swagger
+ * tags:
+ *   name: Employees
+ *   description: The Employee managing API
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     FetchEmployee:
+ *      type: object
+ *      properties:
+  *         id:
+ *           type: string
+ *           description: The auto-generated id of the employee 
+ *         name:
+ *           type: string
+ *           description: Name of the employee
+ *         dob:
+ *           type: DateTime
+ *           description: Date of birth the employee
+ *         gender:
+ *           type: boolean
+ *           description: Gender of the employee
+ *         salary:
+ *           type: decimal
+ *           description: Salary of the employee
+ *         createdDate:
+ *           type: DateTime
+ *           description: Created Date of the employee
+ *         status:
+ *           type: boolean
+ *           description: status of the employee
+ *     Employee:
+ *       type: object
+ *       required:
+ *         - firstname
+ *         - lastname
+ *         - dob
+ *         - gender
+ *         - salary
+ *         - createdDate
+ *         - status
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the employee
+ *         firstname:
+ *           type: string
+ *           description: First Name of the employee
+ *         lastname:
+ *           type: string
+ *           description: Last Name of the employee
+ *         dob:
+ *           type: DateTime
+ *           description: Date of birth the employee
+ *         gender:
+ *           type: boolean
+ *           description: Gender of the employee
+ *         salary:
+ *           type: decimal
+ *           description: Salary of the employee
+ *         createdDate:
+ *           type: DateTime
+ *           description: Created Date of the employee
+ *         status:
+ *           type: boolean
+ *           description: status of the employee
+ *       example:
+ *         firstName: Ali
+ *         lastName: Shah
+ *         dob: 2000-05-05
+ *         gender: true
+ *         salary: 99999
+ *         status: true
+ */
+
+
+/**
+ * @swagger
+ * /api/employee:
+ *   post:
+ *     summary: Create a new Employee
+ *     tags: [Employee]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Employee'
+ *     responses:
+ *       200:
+ *         description: The Employee was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Employee'
+ *       500:
+ *         description: Some server error
+ * 
+ */
 router.post('/', rules, Employee.add);
+
+
+
+/**
+ * @swagger
+ * /api/employee/{id}:
+ *  put:
+ *    summary: Update the book by the id
+ *    tags: [Employee]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The employee id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Employee'
+ *    responses:
+ *      200:
+ *        description: The Employee was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Employee'
+ *      404:
+ *        description: The Employee was not found
+ *      500:
+ *        description: Some error happened
+ */
 router.put('/:id', rules, Employee.update);
+
+
+/**
+ * @swagger
+ * /api/employee:
+ *   get:
+ *     summary: Returns the list of all the Employee
+ *     tags: [Employee]
+ *     responses:
+ *       200:
+ *         description: The list of the Employee
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/FetchEmployee'
+ */
+
 router.get('/', Employee.get);
 
 module.exports = router;
